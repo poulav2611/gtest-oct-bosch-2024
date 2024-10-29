ac6ddc911244113cdd1a847cd2ef03851c7b871f
poulav2611 <poulav.chattopadhaya@in.bosch.com>
2024-10-29 11:05:42 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,152 +1,120 @@
 #include "StringCalculator.h"
-#include<gtest/gtest.h>
-//TEST(TESTSUITE, TestCaseName)
-TEST(string_calculator_add, when_passed_a_single_number_returns_0_for_empty){
-  //Arrange
-  StringCalculator objUndertest;
+#include <gtest/gtest.h>
+
+TEST(string_calculator_ass,when_passed_a_single_number_returns_0){
+//Arrange
+  StringCalculator  objUnderTests;
   string input = "";
   int expectedValue = 0;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
+  input = "0";
+  actualValue = objUnderTests.Add(input);
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add, when_passed_a_single_number_returns_0_for_zero){
-  //Arrange
-  StringCalculator objUndertest;
-  string input = "0";
-  int expectedValue = 0;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
-}
-
-TEST(string_calculator_add, when_passed_a_single_number_returns_1_for_one){
-  //Arrange
-  StringCalculator objUndertest;
+TEST(string_calculator_ass,when_passed_a_single_number_returns_0_for_1){
+//Arrange
+  StringCalculator  objUnderTests;
   string input = "1";
   int expectedValue = 1;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add, when_passed_two_comma_delimited_numbers_returns_their_sum){
-  //Arrange
-  StringCalculator objUndertest;
+TEST(string_calculator_ass,when_passed_two_comma_delimited_numbers){
+//Arrange
+  StringCalculator  objUnderTests;
   string input = "1,2";
   int expectedValue = 3;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add, when_passed_multiple_comma_delimited_numbers_returns_their_sum){
-  //Arrange
-  StringCalculator objUndertest;
-  string input = "1,2,3";
-  int expectedValue = 6;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+TEST(string_calculator_ass,when_delimited_with_newline_and_comma){
+//Arrange
+  StringCalculator  objUnderTests;
+  string input = "1\n2,5";
+  int expectedValue = 8;
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add,when_delimited_with_newline_and_comma_returns_their_sum){
+TEST(string_calculator_ass,when_passed_a_delimiter){
   //Arrange
-  StringCalculator objUndertest;
-  string input = "1\n2,3";
-  int expectedValue = 6;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
-}
-
-TEST(string_calculator_add,when_passed_a_delimiter_returns_the_sum_based_on_that_delimiter){
-  //Arrange
-  StringCalculator objUndertest;
+  StringCalculator  objUnderTests;
   string input = "//;\n1;2";
   int expectedValue = 3;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add,when_passed_numbers_over_1000_ignores_them){
+
+TEST(string_calculator_ass,when_passed_numbers_over_1000){
   //Arrange
-  StringCalculator objUndertest;
+  StringCalculator  objUnderTests;
   string input = "42,1001,3";
   int expectedValue = 45;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add,when_passed_multicharacter_delimiter_uses_that_delimiter_to_sum_values){
+TEST(string_calculator_ass,when_passed_multicharacter_delimiter){
   //Arrange
-  StringCalculator objUndertest;
+  StringCalculator  objUnderTests;
   string input = "//[***]\n8***2***3";
   int expectedValue = 13;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add,when_passed_multiple_delimiters_sums_on_each_delimiter){
+TEST(string_calculator_ass,when_passed_multiple_delimiters){
   //Arrange
-  StringCalculator objUndertest;
+  StringCalculator  objUnderTests;
   string input = "//[*][%]\n4*2%3";
   int expectedValue = 9;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add,when_passed_multiple_multicharacter_delimiters_sums_on_each_delimiter){
+TEST(string_calculator_ass,when_passed_multiple_multicharacter_delimiters){
   //Arrange
-  StringCalculator objUndertest;
-  string input = "//[**][%^]\n4**1%^9";
+  StringCalculator  objUnderTests;
+  string input ="//[**][%^]\n4**1%^9";
   int expectedValue = 14;
-
-  //act
-  int actualValue = objUndertest.Add(input);
-  //Assert
-  ASSERT_EQ(expectedValue, actualValue);
+  //ACT
+  int actualValue = objUnderTests.Add(input);
+  //ASSERT
+  ASSERT_EQ(actualValue,expectedValue);
 }
 
-TEST(string_calculator_add,when_passed_negative_numbers_throws_an_exception_listing_invalid_values){
-  //Arrange
-  StringCalculator objUndertest;
-  string input = "1,-2,-4,5";
-  int expectedValue = 6;
+TEST(string_calculator_add, when_passed_negative_numbers) {
+    StringCalculator objUnderTest;
+    ASSERT_THROW(objUnderTest.Add("1,-2,-4,5"), std::invalid_argument);
+}
 
-  //act
-    ASSERT_THROW({
-      objUndertest.Add(input);
-    }, std::invalid_argument);
-
-    // try {
-    //     calculator.Add("1,-2,-4,5");
-    // } catch (const std::invalid_argument& e) {
-    //     EXPECT_STREQ(e.what(), "Negatives not allowed: -2,-4");
-    // }
+TEST(string_calculator_add, when_passed_negative_numbers_Exception) {
+    StringCalculator objUnderTest;
+    try {
+        objUnderTest.Add("1,-2,-4,5");
+    } catch (const std::invalid_argument& e) {
+        EXPECT_STREQ(e.what(), "Negatives not allowed: -2,-4");
+    }
+}
 }
